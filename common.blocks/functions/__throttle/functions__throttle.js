8553323a5b14948300f6632459c7364b26ce6321
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-04-10 18:45:19 +0400

    move throttle and debounce to separate modules

---
+++ common.blocks/functions/__throttle/functions__throttle.js
@@ -0,0 +1,23 @@
+modules.define('functions.throttle', function(provide) {
+
+provide(function(fn, timeout, ctx) {
+    var timer, args, needInvoke;
+    return function() {
+        args = arguments;
+        needInvoke = true;
+        ctx || (ctx = this);
+
+        timer || (function() {
+            if(needInvoke) {
+                fn.apply(ctx, args);
+                needInvoke = false;
+                timer = setTimeout(arguments.callee, timeout);
+            }
+            else {
+                timer = null;
+            }
+        })();
+    };
+});
+
+});
--- common.blocks/functions/functions.js
+++ common.blocks/functions/functions.js
@@ -8,48 +8,6 @@ var bindCall = function(fn) {
 provide({
     isFunction : function(obj) {
         return toStr(obj) === '[object Function]';
-    },
-
-    debounce : function(fn, timeout, invokeAsap, ctx) {
-        if(arguments.length === 3 && typeof invokeAsap !== 'boolean') {
-            ctx = invokeAsap;
-            invokeAsap = false;
-        }
-
-        var timer;
-        return function() {
-            var args = arguments;
-            ctx || (ctx = this);
-
-            invokeAsap && !timer && fn.apply(ctx, args);
-
-            clearTimeout(timer);
-
-            timer = setTimeout(function() {
-                invokeAsap || fn.apply(ctx, args);
-                timer = null;
-            }, timeout);
-        };
-    },
-
-    throttle : function(fn, timeout, ctx) {
-        var timer, args, needInvoke;
-        return function() {
-            args = arguments;
-            needInvoke = true;
-            ctx || (ctx = this);
-
-            timer || (function() {
-                if(needInvoke) {
-                    fn.apply(ctx, args);
-                    needInvoke = false;
-                    timer = setTimeout(arguments.callee, timeout);
-                }
-                else {
-                    timer = null;
-                }
-            })();
-        };
     }
 });
 
