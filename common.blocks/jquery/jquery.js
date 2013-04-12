0fa3e9f19810360df8c66b0413a02761a64f24eb
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-04-12 23:18:00 +0400

    prevent loading jQuery if it present

--- common.blocks/jquery/jquery.js
+++ common.blocks/jquery/jquery.js
@@ -1,10 +1,12 @@
 modules.define(
     'jQuery',
     ['loader', 'jQuery.config'],
-    function(provide, loader, config) {
+    function(provide, loader, cfg) {
 
-loader(config.url, function() {
-    provide(jQuery);
-});
+typeof jQuery !== 'undefined'?
+    provide(jQuery) :
+    loader(cfg.url, function() {
+        provide(jQuery);
+    });
 
 });
