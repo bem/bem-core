05023da7c7be82d0803749c34c85447c81b24f11
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-05-20 14:20:20 +0400

    update example

--- common.bundles/index/blocks/b-square/b-square.js
+++ common.bundles/index/blocks/b-square/b-square.js
@@ -5,58 +5,20 @@
  *
  * You can find various declarations on the i-bem block's wiki page, blocks/i-bem/i-bem.wiki
  */
-
 modules.define('i-bem__dom', function(provide, DOM) {
 
-provide(DOM.decl('b-square', {
-
-    onSetMod : {
-        'js' : {
-            'inited' : function() {
-                console.log('inited');
-            },
-
-            '' : function() {
-                console.log('destructed');
-            }
-        }
-    },
-
-    onSquareClick : function(e) {
-
-        /*
-         The toggleMod method switches a block's or element's modifier
-         between two values. You can find the method's signature
-         in the BEM reference, /blocks/i-bem/i-bem.jsdoc.wiki
-         */
-
-        this.toggleMod('color', 'green', '');
-
+DOM.decl('b-square', {
+    _onSquareClick : function() {
+        this.toggleMod('color', '', 'green');
     }
-
 }, {
-
     live : function() {
-
-        /*
-         Live initialization is "on-demand initialization".
-         It lets you initialize a block only when necessary,
-         for example, when the user began working with the block directly.
-         In this case, initialization occurs on a click event on the block.
-         Initialization can also occur on an event on a block element,
-         when nested or mixed blocks are initialized. You can find
-         all the available initialization methods in the BEM DOM reference,
-         /blocks/i-bem/dom/i-bem__dom.jsdoc.wiki
-         */
-
-        this.liveBindTo('click', function(e) {
-            this.onSquareClick();
+        this.liveBindTo('click', function() {
+           this._onSquareClick();
         });
-
-        return false;
-
     }
+});
 
-}));
+provide(DOM);
 
 });
