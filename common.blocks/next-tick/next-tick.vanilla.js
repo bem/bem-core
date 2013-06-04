0c37dea4d413ecd247a9ddc5a03a8fea73c40728
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-06-04 19:44:54 +0400

    up next-tick module to 1.0.1

--- common.blocks/next-tick/next-tick.vanilla.js
+++ common.blocks/next-tick/next-tick.vanilla.js
@@ -1,27 +1,21 @@
 /**
- * NextTick module
+ * next-tick module
  *
  * Copyright (c) 2013 Filatov Dmitry (dfilatov@yandex-team.ru)
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
  *
- * @version 1.0.0
+ * @version 1.0.1
  */
 
 modules.define('next-tick', function(provide) {
 
-var global = this;
-
-if(typeof process === 'object') { // nodejs
-    return provide(process.nextTick);
-}
-
-if(global.setImmediate) { // ie10
-    return provide(global.setImmediate);
-}
-
-var fns = [],
+var global = this,
+    fns = [],
+    enqueueFn = function(fn) {
+        return fns.push(fn) === 1;
+    },
     callFns = function() {
         var fnsToCall = fns, i = 0, len = fns.length;
         fns = [];
@@ -30,55 +24,66 @@ var fns = [],
         }
     };
 
-if(global.postMessage) { // modern browsers
-    var isPostMessageAsync = true;
-    if(global.attachEvent) {
-        var checkAsync = function() {
-                isPostMessageAsync = false;
-            };
-        global.attachEvent('onmessage', checkAsync);
-        global.postMessage('__checkAsync', '*');
-        global.detachEvent('onmessage', checkAsync);
-    }
-
-    if(isPostMessageAsync) {
-        var msg = '__nextTick' + +new Date,
-            onMessage = function(e) {
-                if(e.data === msg) {
-                    e.stopPropagation && e.stopPropagation();
-                    callFns();
-                }
-            };
-
-        global.addEventListener?
-            global.addEventListener('message', onMessage, true) :
-            global.attachEvent('onmessage', onMessage);
-
+    if(typeof process === 'object' && process.nextTick) { // nodejs
         return provide(function(fn) {
-            fns.push(fn) === 1 && global.postMessage(msg, '*');
+            enqueueFn(fn) && process.nextTick(callFns);
         });
     }
-}
 
-var doc = global.document;
-if('onreadystatechange' in doc.createElement('script')) { // ie6-ie8
-    var createScript = function() {
-            var script = doc.createElement('script');
-            script.onreadystatechange = function() {
-                script.parentNode.removeChild(script);
-                script = script.onreadystatechange = null;
-                callFns();
+    if(global.setImmediate) { // ie10
+        return provide(function(fn) {
+            enqueueFn(fn) && global.setImmediate(callFns);
+        });
+    }
+
+    if(global.postMessage) { // modern browsers
+        var isPostMessageAsync = true;
+        if(global.attachEvent) {
+            var checkAsync = function() {
+                    isPostMessageAsync = false;
+                };
+            global.attachEvent('onmessage', checkAsync);
+            global.postMessage('__checkAsync', '*');
+            global.detachEvent('onmessage', checkAsync);
+        }
+
+        if(isPostMessageAsync) {
+            var msg = '__nextTick' + +new Date,
+                onMessage = function(e) {
+                    if(e.data === msg) {
+                        e.stopPropagation && e.stopPropagation();
+                        callFns();
+                    }
+                };
+
+            global.addEventListener?
+                global.addEventListener('message', onMessage, true) :
+                global.attachEvent('onmessage', onMessage);
+
+            return provide(function(fn) {
+                enqueueFn(fn) && global.postMessage(msg, '*');
+            });
+        }
+    }
+
+    var doc = global.document;
+    if('onreadystatechange' in doc.createElement('script')) { // ie6-ie8
+        var createScript = function() {
+                var script = doc.createElement('script');
+                script.onreadystatechange = function() {
+                    script.parentNode.removeChild(script);
+                    script = script.onreadystatechange = null;
+                    callFns();
+            };
+            (doc.documentElement || doc.body).appendChild(script);
         };
-        (doc.documentElement || doc.body).appendChild(script);
-    };
 
-    return provide(function(fn) {
-        fns.push(fn) === 1 && createScript();
+        return provide(function(fn) {
+            enqueueFn(fn) && createScript();
+        });
+    }
+
+    provide(function(fn) { // old browsers
+        enqueueFn(fn) && setTimeout(callFns, 0);
     });
-}
-
-return provide(function(fn) { // old browsers
-    setTimeout(fn, 0);
-});
-
 });
