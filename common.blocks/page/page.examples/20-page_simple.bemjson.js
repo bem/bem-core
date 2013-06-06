2fb5509bfcced0529727e4721b50b84e2e24c146
Sergey veged Berezhnoy <veged@mail.ru>
2013-06-06 04:29:12 +0400

    Add block `page` for html/head/body stuff

---
+++ common.blocks/page/page.examples/20-page_simple.bemjson.js
@@ -0,0 +1,10 @@
+({
+    block: 'page',
+    title: 'Page with css file linked',
+    head: [
+        { elem: 'css', url: '_20-page_simple.css' }
+    ],
+    content: [
+        'Page content'
+    ]
+})
