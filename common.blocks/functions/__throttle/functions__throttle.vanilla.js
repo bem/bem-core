345e7a8324c423a865cc32154bda37249a89138b
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-06-06 16:25:23 +0400

    add Asap param

--- common.blocks/functions/__throttle/functions__throttle.vanilla.js
+++ common.blocks/functions/__throttle/functions__throttle.vanilla.js
@@ -1,24 +1,36 @@
 modules.define('functions__throttle', function(provide) {
 
-provide(function(fn, timeout, ctx) {
-    var timer, args, needInvoke;
+provide(function(fn, timeout, invokeAsap, ctx) {
+    var typeofInvokeAsap = typeof invokeAsap;
+    if(typeofInvokeAsap === 'undefined') {
+        invokeAsap = true;
+    }
+    else if(arguments.length === 3 && typeofInvokeAsap !== 'boolean') {
+        ctx = invokeAsap;
+        invokeAsap = true;
+    }
+
+    var timer, args, needInvoke,
+        wrapper = function() {
+            if(needInvoke) {
+                fn.apply(ctx, args);
+                needInvoke = false;
+                timer = setTimeout(wrapper, timeout);
+            }
+            else {
+                timer = null;
+            }
+        };
+
     return function() {
         args = arguments;
-        needInvoke = true;
         ctx || (ctx = this);
+        needInvoke = true;
 
         if(!timer) {
-            var wrapperFn = function() {
-                    if(needInvoke) {
-                        fn.apply(ctx, args);
-                        needInvoke = false;
-                        timer = setTimeout(wrapperFn, timeout);
-                    }
-                    else {
-                        timer = null;
-                    }
-                };
-            wrapperFn();
+            invokeAsap?
+                wrapper() :
+                timer = setTimeout(wrapper, timeout);
         }
     };
 });
