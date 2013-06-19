755bc9d6955d355a4cfdfdf1669010883001b16b
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-06-19 19:07:14 +0400

    Add tests for "next-tick" module (#102)

---
+++ common.blocks/next-tick/next-tick.test.js
@@ -0,0 +1,27 @@
+modules.define('test', ['next-tick'], function(provide, nextTick) {
+
+describe('next-tick', function() {
+    it('should call callback asynchronously', function(done) {
+        var isSync = true;
+        nextTick(function() {
+            isSync.should.be.false;
+            done();
+        });
+        isSync = false;
+    });
+
+    it('should call callbacks in the order of their originating calls', function(done) {
+        var order = [];
+        nextTick(function() { order.push(1) });
+        nextTick(function() { order.push(2) });
+        nextTick(function() { order.push(3) });
+        nextTick(function() {
+            order.should.be.eql([1, 2, 3]);
+            done();
+        });
+    });
+});
+
+provide();
+
+});
