6acf0e2bac6a89a8371fd1543c18de19aef4c2b6
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-06-24 21:56:11 +0400

    Add tests for static inheritance

--- common.blocks/inherit/inherit.test.js
+++ common.blocks/inherit/inherit.test.js
@@ -1,109 +1,178 @@
 modules.define('test', ['inherit'], function(provide, inherit) {
 
 describe('inherit', function() {
-    it('instance should be instance of class', function() {
-        var Cls = inherit({}),
-            instance = new Cls();
+    describe('instance', function() {
+        it('should be instance of class', function() {
+            var Cls = inherit({}),
+                instance = new Cls();
 
-        instance.should.be.instanceOf(Cls);
+            instance.should.be.instanceOf(Cls);
+        });
+
+        it('should be instance of all classes in hierarchy', function() {
+            var ClsA = inherit({}),
+                ClsB = inherit(ClsA, {}),
+                ClsC = inherit(ClsB, {}),
+                instance = new ClsC();
+
+            instance.should.be.instanceOf(ClsA);
+            instance.should.be.instanceOf(ClsB);
+            instance.should.be.instanceOf(ClsC);
+        });
+
+        it('should be instance of constructor return value', function() {
+            var ClsA = inherit({}),
+                ClsB = inherit({
+                    __constructor : function() {
+                        return new ClsA();
+                    }
+                }),
+                instance = new ClsB();
+
+            instance.should.be.instanceOf(ClsA);
+            instance.should.not.be.instanceOf(ClsB);
+        });
+
+        it('instance should have properties from constructor', function() {
+            var Cls = inherit({
+                    __constructor : function() {
+                        this._p1 = 'v1';
+                        this._p2 = 'v2';
+                    }
+                }),
+                instance = new Cls();
+
+            instance._p1.should.be.equal('v1');
+            instance._p2.should.be.equal('v2');
+        });
+
+        it('"__self" property should be pointed to class', function() {
+            var Cls = inherit({}),
+                instance = new Cls();
+
+            instance.__self.should.be.equal(Cls);
+        });
+
+        it('should override methods of base class', function() {
+            var ClsA = inherit({
+                    method1 : function() {
+                        return 'A1';
+                    },
+                    method2 : function() {
+                        return 'A2';
+                    }
+                }),
+                ClsB = inherit(ClsA, {
+                    method1 : function() {
+                        return 'B1';
+                    }
+                }),
+                ClsC = inherit(ClsB, {
+                    method2 : function() {
+                        return 'C2';
+                    }
+                }),
+                instance = new ClsC();
+
+            instance.method1().should.be.equal('B1');
+            instance.method2().should.be.equal('C2');
+        });
+
+        it('__base should call methods of base class', function() {
+            var ClsA = inherit({
+                    method1 : function() {
+                        return 'A1';
+                    },
+                    method2 : function() {
+                        return 'A2';
+                    }
+                }),
+                ClsB = inherit(ClsA, {
+                    method1 : function() {
+                        return this.__base() + 'B1';
+                    }
+                }),
+                ClsC = inherit(ClsB, {
+                    method1 : function() {
+                        return this.__base() + 'C1';
+                    },
+
+                    method2 : function() {
+                        return this.__base() + 'C2';
+                    }
+                }),
+                instance = new ClsC();
+
+            instance.method1().should.be.equal('A1B1C1');
+            instance.method2().should.be.equal('A2C2');
+        });
     });
 
-    it('instance should be instance of all classes in hierarchy', function() {
-        var ClsA = inherit({}),
-            ClsB = inherit(ClsA, {}),
-            ClsC = inherit(ClsB, {}),
-            instance = new ClsC();
-
-        instance.should.be.instanceOf(ClsA);
-        instance.should.be.instanceOf(ClsB);
-        instance.should.be.instanceOf(ClsC);
-    });
-
-    it('instance should be instance of constructor return value', function() {
-        var ClsA = inherit({}),
-            ClsB = inherit({
-                __constructor : function() {
-                    return new ClsA();
-                }
-            }),
-            instance = new ClsB();
-
-        instance.should.be.instanceOf(ClsA);
-        instance.should.not.be.instanceOf(ClsB);
-    });
-
-    it('instance should have properties from constructor', function() {
-        var Cls = inherit({
-                __constructor : function() {
-                    this._p1 = 'v1';
-                    this._p2 = 'v2';
-                }
-            }),
-            instance = new Cls();
-
-        instance._p1.should.be.equal('v1');
-        instance._p2.should.be.equal('v2');
-    });
-
-    it('instance "__self" property should be pointed to class', function() {
-        var Cls = inherit({}),
-            instance = new Cls();
-
-        instance.__self.should.be.equal(Cls);
-    });
-
-    it('should override methods of base class', function() {
-        var ClsA = inherit({
-                method1 : function() {
-                    return 'A1';
+    describe('static', function() {
+        it('properties should be assigned', function() {
+            var Cls = inherit({}, {
+                method : function() {
+                    return 'method';
                 },
-                method2 : function() {
-                    return 'A2';
-                }
-            }),
-            ClsB = inherit(ClsA, {
-                method1 : function() {
-                    return 'B1';
-                }
-            }),
-            ClsC = inherit(ClsB, {
-                method2 : function() {
-                    return 'C2';
-                }
-            }),
-            instance = new ClsC();
 
-        instance.method1().should.be.equal('B1');
-        instance.method2().should.be.equal('C2');
-    });
+                prop : 'val'
+            });
 
-    it('__base should call methods of base class', function() {
-        var ClsA = inherit({
-                method1 : function() {
-                    return 'A1';
-                },
-                method2 : function() {
-                    return 'A2';
-                }
-            }),
-            ClsB = inherit(ClsA, {
-                method1 : function() {
-                    return this.__base() + 'B1';
-                }
-            }),
-            ClsC = inherit(ClsB, {
-                method1 : function() {
-                    return this.__base() + 'C1';
-                },
+            Cls.method().should.be.equal('method');
+            Cls.prop.should.be.equal('val');
+        });
 
-                method2 : function() {
-                    return this.__base() + 'C2';
-                }
-            }),
-            instance = new ClsC();
+        it('properties should override properties of base class', function() {
+            var ClsA = inherit({}, {
+                    method1 : function() {
+                        return 'A1';
+                    },
+                    method2 : function() {
+                        return 'A2';
+                    }
+                }),
+                ClsB = inherit(ClsA, {}, {
+                    method1 : function() {
+                        return 'B1';
+                    }
+                }),
+                ClsC = inherit(ClsB, {}, {
+                    method2 : function() {
+                        return 'C2';
+                    }
+                });
 
-        instance.method1().should.be.equal('A1B1C1');
-        instance.method2().should.be.equal('A2C2');
+            ClsC.method1().should.be.equal('B1');
+            ClsC.method2().should.be.equal('C2');
+        });
+
+        it('__base should call methods of base class', function() {
+            var ClsA = inherit({}, {
+                    method1 : function() {
+                        return 'A1';
+                    },
+                    method2 : function() {
+                        return 'A2';
+                    }
+                }),
+                ClsB = inherit(ClsA, {}, {
+                    method1 : function() {
+                        return this.__base() + 'B1';
+                    }
+                }),
+                ClsC = inherit(ClsB, {}, {
+                    method1 : function() {
+                        return this.__base() + 'C1';
+                    },
+
+                    method2 : function() {
+                        return this.__base() + 'C2';
+                    }
+                });
+
+            ClsC.method1().should.be.equal('A1B1C1');
+            ClsC.method2().should.be.equal('A2C2');
+        });
     });
 });
 
