20aa6f089507e3c6155a16f89079ea8bd574f6d5
aristov <vv.aristov@gmail.com>
2013-07-04 19:36:00 +0400

    move some changes from i-bem__elems_type_dom to i-bem__dom_elems_yes

--- common.blocks/i-bem/__dom/_elems/i-bem__dom_elems_yes.js
+++ common.blocks/i-bem/__dom/_elems/i-bem__dom_elems_yes.js
@@ -14,6 +14,7 @@ var buildClass = INTERNAL.buildClass,
 
 /**
  * Clears the element's modifiers cache
+ * TODO костыль, сделать с использованием кэша
  */
 function clearElemModCache() {
     var result = this.__base.apply(this, arguments);
@@ -165,51 +166,6 @@ BEM.decl('i-bem__dom', {
 }, {
 
     /**
-     * Declares elements and creates an elements class
-     * @static
-     * @protected
-     * @param {Object} decl Element description
-     * @param {String} decl.block Block name
-     * @param {String} decl.elem Element name
-     * @param {String} [decl.baseBlock] Name of the parent block
-     * @param {String} [decl.modName] Modifier name
-     * @param {String} [decl.modVal] Modifier value
-     * @param {Object} [props] Methods
-     * @param {Object} [staticProps] Static methods
-     */
-    decl : function(decl, props, staticProps) {
-        var block;
-        if (decl.elem) {
-            block = this.__base(
-                {
-                    block: buildClass(decl.block, decl.elem),
-                    baseBlock: decl.baseBlock,
-                    modName: decl.modName,
-                    modVal: decl.modVal
-                },
-                props,
-                staticProps);
-            block._blockName = decl.block;
-            block._elemName = decl.elem;
-        } else {
-            block = this.__base.apply(this, arguments);
-            block._blockName = block._name;
-        }
-        return block;
-    },
-
-    /**
-     * Returns the name of the current instance
-     * @static
-     * @protected
-     * @param {Boolean} [shortName] return the short name for current instance
-     * @returns {String}
-     */
-    getName : function(shortName) {
-        return shortName? (this._elemName || this._blockName) : this._name;
-    },
-
-    /**
      * Helper for live initialization for a parent block's event
      * @static
      * @protected
@@ -272,6 +228,6 @@ BEM.decl('i-bem__dom', {
 
 });
 
-provide(BEM.blocks['i-bem__dom']);
+provide(DOM);
 
 });
