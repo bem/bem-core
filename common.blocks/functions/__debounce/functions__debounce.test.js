f2ce62c4c9c3249e0f164272a5850f1ac285e190
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-07-19 14:46:09 +0400

    functions__debounce.tests: increase timeouts

--- common.blocks/functions/__debounce/functions__debounce.test.js
+++ common.blocks/functions/__debounce/functions__debounce.test.js
@@ -7,7 +7,7 @@ describe('functions__debounce', function() {
                 function(arg) {
                     res.push(arg);
                 },
-                10);
+                50);
 
         debouncedFn(1);
         debouncedFn(2);
@@ -15,15 +15,15 @@ describe('functions__debounce', function() {
         setTimeout(function() {
             debouncedFn(4);
             debouncedFn(5);
-        }, 5);
+        }, 25);
         setTimeout(function() {
             debouncedFn(6);
             debouncedFn(7);
-        }, 45);
+        }, 220);
         setTimeout(function() {
             res.should.be.eql([5, 7]);
             done();
-        }, 80);
+        }, 300);
     });
 
     it('should properly debounce given function according "invokeAsap" param', function(done) {
@@ -32,7 +32,7 @@ describe('functions__debounce', function() {
                 function(arg) {
                     res.push(arg);
                 },
-                10,
+                50,
                 true);
 
         debouncedFn(1);
@@ -41,15 +41,15 @@ describe('functions__debounce', function() {
         setTimeout(function() {
             debouncedFn(4);
             debouncedFn(5);
-        }, 5);
+        }, 25);
         setTimeout(function() {
             debouncedFn(6);
             debouncedFn(7);
-        }, 45);
+        }, 230);
         setTimeout(function() {
             res.should.be.eql([1, 6]);
             done();
-        }, 80);
+        }, 400);
     });
 
     it('should call debounced function with given "ctx" param', function(done) {
