a263541bfbdec51384debedf4c149006945323b0
Sergey veged Berezhnoy <veged@mail.ru>
2013-07-19 17:20:35 +0400

    Add strings__escape module

---
+++ common.blocks/strings/__escape/strings__escape.vanilla.js
@@ -0,0 +1,23 @@
+modules.define('strings__escape', function(provide) {
+
+var symbols = {
+        '"' : '&quot;',
+        "'" : '&apos;',
+        '&' : '&amp;',
+        '<' : '&lt;',
+        '>' : '&gt;'
+    },
+    mapSymbol = function(s) { return symbols[s] || s },
+    buildEscape = function(regexp) {
+        regexp = new RegExp(regexp, 'g');
+        return function(s) { return ('' + s).replace(regexp, mapSymbol) }
+    };
+
+provide({
+    xml : buildEscape('[&<>]'),
+    html : buildEscape('[&<>]'),
+    attr : buildEscape('["\'&<>]')
+});
+
+});
+
