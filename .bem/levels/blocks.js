77635c8e5c8b69fc357ae09bd17214bde74c02e0
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-08-02 13:39:34 +0400

    Use bem-environ

--- .bem/levels/blocks.js
+++ .bem/levels/blocks.js
@@ -1,21 +1,26 @@
 var PATH = require('path'),
     BEM = require('bem'),
-    environ = require('../environ'),
+    environ = require('bem-environ'),
 
     PRJ_TECHS = PATH.resolve(__dirname, '../techs'),
     join = PATH.join;
 
 exports.getTechs = function() {
     var techs = {
-        'bemjson.js'                 : '',
-        'bemdecl.js'                 : 'bemdecl.js',
-        'deps.js'                    : 'deps.js',
-        'css'                        : 'css',
-        'ie.css'                     : 'ie.css',
-        'js'                         : 'js-i',
-        'test.js'                    : environ.getLibPath('bem-pr', 'bem/techs/test.js.js'),
-        'test.js+browser.js+bemhtml' : environ.getLibPath('bem-pr', 'bem/techs/test.js+browser.js+bemhtml.js')
+        'bemjson.js' : '',
+        'bemdecl.js' : 'bemdecl.js',
+        'deps.js'    : 'deps.js',
+        'css'        : 'css',
+        'ie.css'     : 'ie.css',
+        'js'         : 'js-i'
     };
+
+    [
+        'test.js',
+        'test.js+browser.js+bemhtml'
+    ].forEach(function(name) {
+        techs[name] = environ.getLibPath('bem-pr', 'bem', 'techs', name + '.js');
+    });
 
     [
         'bemhtml',
