d34fe29ba3423044a0feff2880bf4104d3312b6e
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-08-12 10:46:56 +0400

    functions: add jsdoc, add test for noop

--- common.blocks/functions/functions.test.js
+++ common.blocks/functions/functions.test.js
@@ -1,16 +1,24 @@
 modules.define('test', ['functions'], function(provide, functions) {
 
-describe('isFunction', function() {
-    it('should returns true only for function', function() {
-        functions.isFunction({}).should.be.false;
-        functions.isFunction(null).should.be.false;
-        functions.isFunction(5).should.be.false;
-        functions.isFunction().should.be.false;
-        functions.isFunction('').should.be.false;
-        functions.isFunction([]).should.be.false;
-        functions.isFunction(new function() {}).should.be.false;
+describe('functions', function() {
+    describe('isFunction', function() {
+        it('should returns true only for function', function() {
+            functions.isFunction({}).should.be.false;
+            functions.isFunction(null).should.be.false;
+            functions.isFunction(5).should.be.false;
+            functions.isFunction().should.be.false;
+            functions.isFunction('').should.be.false;
+            functions.isFunction([]).should.be.false;
+            functions.isFunction(new function() {}).should.be.false;
 
-        functions.isFunction(function() {}).should.be.true;
+            functions.isFunction(function() {}).should.be.true;
+        });
+    });
+
+    describe('noop', function() {
+        it('should be a function', function() {
+            functions.isFunction(functions.noop).should.be.true;
+        });
     });
 });
 
--- common.blocks/functions/functions.vanilla.js
+++ common.blocks/functions/functions.vanilla.js
@@ -7,10 +7,18 @@ modules.define('functions', function(provide) {
 var toStr = Object.prototype.toString;
 
 provide({
+    /**
+     * Checks whether a given object is function
+     * @param {*} obj
+     * @returns {Boolean}
+     */
     isFunction : function(obj) {
         return toStr.call(obj) === '[object Function]';
     },
-    
+
+    /**
+     * @type {Function}
+     */
     noop : function() {}
 });
 
