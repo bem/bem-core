26763f01017828604e0c6a20539fdce27be2d6af
Serhii Tatarintsev <sevinf@yandex-team.ru>
2013-08-12 19:52:01 +0300

    browser.js+bemhtml appends bemhtml to the bottom

--- .bem/techs/browser.js+bemhtml.js
+++ .bem/techs/browser.js+bemhtml.js
@@ -48,12 +48,12 @@ exports.techMixin = {
                 return bemhtmlResults
                     .then(function(r) {
 
-                        // put bemhtml templates at the top of builded js file
+                        // put bemhtml templates at the bottom of builded js file
                         Object.keys(res).forEach(function(suffix) {
                             // test for array as in i18n.js+bemhtml tech
                             // there's hack to create symlink for default lang
                             // so 'js' key is a string there
-                            Array.isArray(res[suffix]) && res[suffix].unshift(r['bemhtml.js']);
+                            Array.isArray(res[suffix]) && res[suffix].push(r['bemhtml.js']);
                         });
 
                     });
