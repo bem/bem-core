fbad693a9bcf4243a5150f017745e7e980ed2bcf
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-08-12 11:00:48 +0400

    objects: add jsdoc

--- common.blocks/objects/objects.vanilla.js
+++ common.blocks/objects/objects.vanilla.js
@@ -7,7 +7,13 @@ modules.define('objects', function(provide) {
 var hasOwnProp = Object.prototype.hasOwnProperty;
 
 provide({
-    extend : function(target) {
+    /**
+     * Extends a given target by
+     * @param {Object} target object to extend
+     * @param {...Object} source
+     * @returns {Object}
+     */
+    extend : function(target, source) {
         typeof target !== 'object' && (target = {});
 
         for(var i = 1, len = arguments.length; i < len; i++) {
@@ -22,6 +28,11 @@ provide({
         return target;
     },
 
+    /**
+     * Check whether a given object is empty (contains no enumerable properties)
+     * @param {Object} obj
+     * @returns {Boolean}
+     */
     isEmpty : function(obj) {
         for(var key in obj) {
             if(hasOwnProp.call(obj, key)) {
@@ -32,6 +43,12 @@ provide({
         return true;
     },
 
+    /**
+     * Generic iterator function over object
+     * @param {Object} obj object to iterate
+     * @param {Function} fn callback
+     * @param {Object} [ctx] callbacks's context
+     */
     each : function(obj, fn, ctx) {
         for(var key in obj) {
             if(hasOwnProp.call(obj, key)) {
