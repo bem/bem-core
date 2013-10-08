632d94146c2e9d460d132f5ca1147381fd01c5a8
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-10-08 13:40:55 +0400

    page: get rid `meta` field in flavor of explicit use `meta` elem in `head` field

--- common.blocks/page/page.examples/10-simple.bemjson.js
+++ common.blocks/page/page.examples/10-simple.bemjson.js
@@ -2,10 +2,10 @@
     block : 'page',
     title : 'Простой пример html/head/body-обвязки страницы',
     favicon : '//bem.info/favicon.ico',
-    meta : { elem : 'meta', attrs : { name : 'keywords', content : 'BEM' } },
     head : [
         { elem : 'css', url : '_10-simple.css' },
-        { elem : 'js', url : '_10-simple.js' }
+        { elem : 'js', url : '_10-simple.js' },
+        { elem : 'meta', attrs : { name : 'keywords', content : 'BEM' } }
     ],
     content : { block : 'bla', content : 'bla' }
 })
