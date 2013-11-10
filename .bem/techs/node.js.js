c539b69bca0d0a798e64a8649ebe797b95a6d5b2
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-11-10 20:28:43 +0400

    Generated module paths should respect windows systems (fix #274)

--- .bem/techs/node.js.js
+++ .bem/techs/node.js.js
@@ -1,3 +1,5 @@
+var PATH = require('path');
+
 exports.baseTechName = 'vanilla.js';
 
 exports.techMixin = {
@@ -29,12 +31,18 @@ exports.techMixin = {
         ].join('');
     },
 
-    getBuildResultChunk : function(relPath, path) {
-        var fChar = relPath.charAt(0);
-        if(fChar !== '/' && fChar !== '.') {
+    getBuildResultChunk : function(relPath) {
+        // NOTE: module's path is always an UNIX path,
+        // so `relPath` should be converted on Windows systems
+        if(PATH.sep !== '/') {
+            relPath = relPath.replace(/\\/g, '/');
+        }
+        // NOTE: Without a leading '/' or './' to indicate a file, the module is either
+        // a "core module" or is loaded from a node_modules folder
+        if(/^\w/.test(relPath)) {
             relPath = './' + relPath;
         }
-        return "require('" + relPath + "');\n";
+        return 'require(\'' + relPath + '\');\n';
     }
 
 };
