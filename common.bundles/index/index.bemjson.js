bc0bcba77e9b22ea64dade4e38293413c583646d
Mikhail Troshev <mishanga@yandex-team.ru>
2014-02-04 16:48:11 +0400

    page: shortcuts for styles and scripts

--- common.bundles/index/index.bemjson.js
+++ common.bundles/index/index.bemjson.js
@@ -1,15 +1,13 @@
 ({
     block: 'page',
     title: 'Обработчик события click',
-    head: [
-        { elem: 'css', url: '_index.css', ie: false },
-        { elem: 'css', url: '_index.ie.css', ie: 'lte IE 9' }
-    ],
+    styles: { elem: 'css', url: '_index.css' },
+    scripts: { elem: 'js', url: '_index.js' },
     content: [{
         block: 'b-square',
         js: {id:1}
     },{
         block: 'b-square',
         js: {id:1}
-    },{ elem: 'js', url: '_index.js' }]
+    }]
 });
