d81ee37d04a3f8fcc3eade7951be7fb7a1f81671
Vladimir Varankin <nek.narqo+git@gmail.com>
2014-04-12 15:57:45 +0400

    Keyboard helpers (closes #431)

---
+++ common.blocks/keyboard/__codes/keyboard__codes.js
@@ -0,0 +1,25 @@
+/**
+ * @module keyboard__codes
+ */
+modules.define('keyboard__codes', function(provide) {
+
+provide(/** @exports */{
+    BACKSPACE : 8,
+    TAB : 9,
+    ENTER : 13,
+    CAPS_LOCK : 20,
+    ESC : 27,
+    SPACE : 32,
+    PAGE_UP : 33,
+    PAGE_DOWN : 34,
+    END : 35,
+    HOME : 36,
+    LEFT : 37,
+    UP : 38,
+    RIGHT : 39,
+    DOWN : 40,
+    INSERT : 41,
+    DELETE : 42
+});
+
+});
