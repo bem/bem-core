ffab26a1d8090a51b97d164476052e8c4ade3ec8
seles <seles@yandex-team.ru>
2015-03-04 18:31:02 +0300

    ua: Add nonce attribute #882

---
+++ common.blocks/page/page.tmpl-specs/40-nonce.bemjson.js
@@ -0,0 +1,13 @@
+({
+    block : 'page',
+    scripts : [
+        { elem : 'js', url : 'http://yastatic.net/jquery/2.1.1/jquery.min.js' },
+        { elem : 'js', url : 'http://yastatic.net/jquery/easing/1.3/jquery.easing.min.js' },
+        { elem : 'js', content : 'var a = true;' }
+    ],
+    nonce : '123',
+    content : {
+        block : 'bla',
+        content : 'bla-bla'
+    }
+})
