5e1ca2750dec701d3a7d731fbe025738a72d0eaa
Evgeniy Konstantinov <sipayrt@yandex-team.ru>
2015-05-14 18:10:41 +0300

    Update JSDoc to fit new version of jsd

--- common.blocks/keyboard/__codes/keyboard__codes.js
+++ common.blocks/keyboard/__codes/keyboard__codes.js
@@ -4,21 +4,37 @@
 modules.define('keyboard__codes', function(provide) {
 
 provide(/** @exports */{
+    /** @type {Number} */
     BACKSPACE : 8,
+    /** @type {Number} */
     TAB : 9,
+    /** @type {Number} */
     ENTER : 13,
+    /** @type {Number} */
     CAPS_LOCK : 20,
+    /** @type {Number} */
     ESC : 27,
+    /** @type {Number} */
     SPACE : 32,
+    /** @type {Number} */
     PAGE_UP : 33,
+    /** @type {Number} */
     PAGE_DOWN : 34,
+    /** @type {Number} */
     END : 35,
+    /** @type {Number} */
     HOME : 36,
+    /** @type {Number} */
     LEFT : 37,
+    /** @type {Number} */
     UP : 38,
+    /** @type {Number} */
     RIGHT : 39,
+    /** @type {Number} */
     DOWN : 40,
+    /** @type {Number} */
     INSERT : 45,
+    /** @type {Number} */
     DELETE : 46
 });
 
