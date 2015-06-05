96ae2250fa039b12f1dbea957106b9f6d567e242
Vladimir Varankin <nek.narqo+git@gmail.com>
2015-06-05 14:27:47 +0300

    identify: Get rid of onlyGet argument
    closes #1028

--- common.blocks/identify/identify.vanilla.js
+++ common.blocks/identify/identify.vanilla.js
@@ -8,24 +8,42 @@ var counter = 0,
     expando = '__' + (+new Date),
     get = function() {
         return 'uniq' + (++counter);
+    },
+    identify = function(obj) {
+        if((typeof obj === 'object' && obj !== null) || typeof obj === 'function') {
+            var key = 'uniqueID' in obj?
+                'uniqueID' :
+                expando; // Use when possible native uniqueID for elements in IE
+
+            return key in obj?
+                obj[key] :
+                obj[key] = get();
+        }
+
+        return '';
     };
 
 provide(
     /**
      * Makes unique ID
      * @exports
-     * @param {Object} obj Object that needs to be identified
-     * @param {Boolean} [onlyGet=false] Return a unique value only if it had already been assigned before
+     * @param {Object} [obj...] Object that needs to be identified
      * @returns {String} ID
      */
-    function(obj, onlyGet) {
-        if(!obj) return get();
+    function(obj) {
+        if(arguments.length) {
+            if(arguments.length === 1) {
+                return identify(obj);
+            }
 
-        var key = 'uniqueID' in obj? 'uniqueID' : expando; // Use when possible native uniqueID for elements in IE
+            var res = '';
+            for(var i = 0, len = arguments.length; i < len; i++) {
+                res += identify(arguments[i]);
+            }
+            return res;
+        }
 
-        return onlyGet || key in obj?
-            obj[key] :
-            obj[key] = get();
+        return get();
     }
 );
 
