f348148f3d73fba613eece1a7a140ec375358a1d
Vladimir Grinenko <i@tadatuta.com>
2015-09-06 23:55:00 +0300

    Improve bem create usage #1183

--- .bem/techs/browser.js.js
+++ .bem/techs/browser.js.js
@@ -24,6 +24,33 @@ exports.techMixin = {
         };
     },
 
+    getCreateResult : function(path, suffix, vars) {
+        var moduleName = vars.BlockName;
+        vars.ElemName &&
+            (moduleName += '__' + vars.ElemName);
+        vars.ModName &&
+            (moduleName += '_' + vars.ModName);
+        vars.ModVal &&
+            (moduleName += '_' + vars.ModVal);
+        vars.ModuleName = moduleName;
+
+        return BEM.template.process([
+            'modules.define(\'{{bemModuleName}}\', [\'i-bem__dom\'], function(provide, BEMDOM) {',
+            '',
+            'provide(BEMDOM.decl(this.name, {',
+            '    onSetMod: {',
+            '        js: {',
+            '            inited: function() {',
+            '',
+            '            }',
+            '        }',
+            '    }',
+            '}));',
+            '',
+            '})'
+        ], vars);
+    },
+
     getYmChunk : function() {
         var ymRelPath = this.__base.apply(this, arguments);
         return this.getBuildResultChunk(ymRelPath);
