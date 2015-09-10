e0bca4ac308cd4e077ee8204cf0fb2ddbaaa21eb
Vladimir Grinenko <i@tadatuta.com>
2015-09-10 21:42:06 +0300

    Support migration from underscored assets to bundle.min.ext form

--- common.blocks/page/page.tmpl-specs/10-simple.bemjson.js
+++ common.blocks/page/page.tmpl-specs/10-simple.bemjson.js
@@ -3,8 +3,8 @@
     title : 'Простой пример html/head/body-обвязки страницы',
     favicon : '//bem.info/favicon.ico',
     head : [
-        { elem : 'css', url : '_10-simple.css' },
-        { elem : 'js', url : '_10-simple.js' },
+        { elem : 'css', url : '10-simple.css' },
+        { elem : 'js', url : '10-simple.js' },
         { elem : 'meta', attrs : { name : 'keywords', content : 'BEM' } }
     ],
     content : { block : 'bla', content : 'bla' }
