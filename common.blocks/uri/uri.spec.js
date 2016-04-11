04494371110d3cffd67ae48e2cdba68beb87e2f9
Sergey Berezhnoy <veged@yandex-team.ru>
2016-04-11 19:30:02 +0300

    querystring: Move to uri__querystring (closes #967)

--- common.blocks/uri/uri.spec.js
+++ common.blocks/uri/uri.spec.js
@@ -1,32 +1,28 @@
-modules.define('spec', ['querystring__uri'], function(provide, qs) {
+modules.define('spec', ['uri'], function(provide, uri) {
 
-describe('querystring__uri', function() {
+describe('uri', function() {
     describe('decodeURIComponent()', function() {
-
         it('should be able to decode cp1251 encoded params', function() {
-            qs.decodeURIComponent('%F2%E0%E1%EB%EE').should.be.eql('табло');
+            uri.decodeURIComponent('%F2%E0%E1%EB%EE').should.be.eql('табло');
         });
 
         it('should not fall on params encoded with unknown encoding', function() {
-            qs.decodeURIComponent('%COCO%C0C0').should.be.eql('%COCO%C0C0');
+            uri.decodeURIComponent('%COCO%C0C0').should.be.eql('%COCO%C0C0');
         });
-
     });
 
     describe('decodeURI()', function() {
-
         it('should be able to decode url with cp1251 encoded params', function() {
-            qs
+            uri
                 .decodeURI('http://test.com/ololo/trololo.html?text=%F2%E0%E1%EB%EE')
                 .should.be.eql('http://test.com/ololo/trololo.html?text=табло');
         });
 
         it('should not fall on url with params encoded with unknown encoding', function() {
-            qs
+            uri
                 .decodeURI('http://test.com/ololo/trololo.html?text=%COCO%C0C0')
                 .should.be.eql('http://test.com/ololo/trololo.html?text=%COCO%C0C0');
         });
-
     });
 });
 
