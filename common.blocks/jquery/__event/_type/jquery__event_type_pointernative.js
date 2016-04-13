ffca220f701ea2b77348fd6e60418b52476f5424
GreLI <grelimail@gmail.com>
2016-04-13 21:14:28 +0300

    Fixed “Expected identifier” error in IE8

--- common.blocks/jquery/__event/_type/jquery__event_type_pointernative.js
+++ common.blocks/jquery/__event/_type/jquery__event_type_pointernative.js
@@ -139,7 +139,7 @@ function SparseArrayMap() {
 SparseArrayMap.prototype = {
     set : function(k, v) {
         if(v === undef) {
-            return this.delete(k);
+            return this['delete'](k);
         }
         if(!this.has(k)) {
             this.size++;
@@ -151,7 +151,7 @@ SparseArrayMap.prototype = {
         return this.array[k] !== undef;
     },
 
-    delete : function(k) {
+    'delete' : function(k) {
         if(this.has(k)){
             delete this.array[k];
             this.size--;
