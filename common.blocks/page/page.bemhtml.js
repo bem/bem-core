9d91661b660ef295943968e00060586726e98d31
Vladimir Grinenko <i@tadatuta.com>
2016-04-13 23:44:12 +0300

    page: Refactor BEMHTML template. Closes #1311

--- common.blocks/page/page.bemhtml.js
+++ common.blocks/page/page.bemhtml.js
@@ -1,40 +1,37 @@
 block('page')(
 
-    def().match(function() { return !this._pageInit;})(function() {
+    wrap()(function() {
         var ctx = this.ctx;
         this._nonceCsp = ctx.nonce;
 
-        // TODO(indunty): remove local after bem/bem-xjst#50
-        return local({ _pageInit : true })(function() {
-            return applyCtx([
-                ctx.doctype || '<!DOCTYPE html>',
-                {
-                    tag : 'html',
-                    cls : 'ua_js_no',
-                    content : [
-                        {
-                            elem : 'head',
-                            content : [
-                                { tag : 'meta', attrs : { charset : 'utf-8' } },
-                                ctx.uaCompatible === false? '' : {
-                                    tag : 'meta',
-                                    attrs : {
-                                        'http-equiv' : 'X-UA-Compatible',
-                                        content : ctx.uaCompatible || 'IE=edge'
-                                    }
-                                },
-                                { tag : 'title', content : ctx.title },
-                                { block : 'ua', attrs : { nonce : ctx.nonce } },
-                                ctx.head,
-                                ctx.styles,
-                                ctx.favicon? { elem : 'favicon', url : ctx.favicon } : ''
-                            ]
-                        },
-                        ctx
-                    ]
-                }
-            ]);
-        });
+        return [
+            ctx.doctype || '<!DOCTYPE html>',
+            {
+                tag : 'html',
+                cls : 'ua_js_no',
+                content : [
+                    {
+                        elem : 'head',
+                        content : [
+                            { tag : 'meta', attrs : { charset : 'utf-8' } },
+                            ctx.uaCompatible === false? '' : {
+                                tag : 'meta',
+                                attrs : {
+                                    'http-equiv' : 'X-UA-Compatible',
+                                    content : ctx.uaCompatible || 'IE=edge'
+                                }
+                            },
+                            { tag : 'title', content : ctx.title },
+                            { block : 'ua', attrs : { nonce : ctx.nonce } },
+                            ctx.head,
+                            ctx.styles,
+                            ctx.favicon? { elem : 'favicon', url : ctx.favicon } : ''
+                        ]
+                    },
+                    ctx
+                ]
+            }
+        ];
     }),
 
     tag()('body'),
