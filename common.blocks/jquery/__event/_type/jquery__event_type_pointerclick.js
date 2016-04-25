6c576445d62887c590648d26819adda1ba9fce40
Sergey Berezhnoy <veged@yandex-team.ru>
2016-04-25 18:11:39 +0300

    new implementation of pointerclick for IOS only (closes #1253)

--- common.blocks/jquery/__event/_type/jquery__event_type_pointerclick.js
+++ common.blocks/jquery/__event/_type/jquery__event_type_pointerclick.js
@@ -1,22 +1,62 @@
-modules.define('jquery', function(provide, $) {
+modules.define('jquery', ['next-tick'], function(provide, nextTick, $) {
 
-var event = $.event.special.pointerclick = {
-    setup : function() {
-        $(this).on('click', event.handler);
-    },
+var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream,
+    event = $.event.special.pointerclick = {
+        setup : function() {
+            if(isIOS) {
+                $(this)
+                    .on('pointerdown', event.onPointerdown)
+                    .on('pointerup', event.onPointerup)
+                    .on('pointerleave pointercancel', event.onPointerleave);
+            } else {
+                $(this).on('click', event.handler);
+            }
+        },
 
-    teardown : function() {
-        $(this).off('click', event.handler);
-    },
+        teardown : function() {
+            if(isIOS) {
+                $(this)
+                    .off('pointerdown', event.onPointerdown)
+                    .off('pointerup', event.onPointerup)
+                    .off('pointerleave pointercancel', event.onPointerleave);
+            } else {
+                $(this).off('click', event.handler);
+            }
+        },
 
-    handler : function(e) {
-        if(!e.button) {
-            e.type = 'pointerclick';
-            $.event.dispatch.apply(this, arguments);
-            e.type = 'click';
+        handler : function(e) {
+            if(!e.button) {
+                var type = e.type;
+                e.type = 'pointerclick';
+                $.event.dispatch.apply(this, arguments);
+                e.type = type;
+            }
+        },
+
+        onPointerdown : function(e) {
+            pointerdownEvent = e;
+        },
+
+        onPointerleave : function() {
+            pointerdownEvent = null;
+        },
+
+        onPointerup : function(e) {
+            if(!pointerdownEvent) return;
+
+            if(!pointerDownUpInProgress) {
+                nextTick(function() {
+                    pointerDownUpInProgress = false;
+                    pointerdownEvent = null;
+                });
+                pointerDownUpInProgress = true;
+            }
+
+            event.handler.apply(this, arguments);
         }
-    }
-};
+    },
+    pointerDownUpInProgress = false,
+    pointerdownEvent;
 
 provide($);
 
