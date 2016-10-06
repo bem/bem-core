7bd714ae960bcb4276b3bab27664333db824f248
Vladimir Grinenko <i@tadatuta.com>
2016-10-07 01:28:29 +0300

    ua: Fix templates

--- common.blocks/ua/__svg/ua__svg.bh.js
+++ common.blocks/ua/__svg/ua__svg.bh.js
@@ -1,14 +1,17 @@
 module.exports = function(bh) {
-
     bh.match('ua', function(ctx, json) {
         ctx.applyBase();
         ctx.content([
             json.content,
-            '(function(d,n){',
-                'd.documentElement.className+=',
-                '" ua_svg_"+(d[n]&&d[n]("http://www.w3.org/2000/svg","svg").createSVGRect?"yes":"no");',
-            '})(document,"createElementNS");'
+            {
+                tag : false,
+                html : [
+                    '(function(d,n){',
+                        'd.documentElement.className+=',
+                        '" ua_svg_"+(d[n]&&d[n]("http://www.w3.org/2000/svg","svg").createSVGRect?"yes":"no");',
+                    '})(document,"createElementNS");'
+                ].join('')
+            }
         ], true);
     });
-
 };
