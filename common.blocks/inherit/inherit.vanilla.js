2cfcf0f0e67415e20125d261e881396f5cd12ada
Ilya Rogov <ilyar.software@gmail.com>
2017-08-11 01:03:18 +0300

    Update inherit to last stable version

--- common.blocks/inherit/inherit.vanilla.js
+++ common.blocks/inherit/inherit.vanilla.js
@@ -1,12 +1,14 @@
 /**
  * @module inherit
+ * @version 2.2.6
+ * @author Filatov Dmitry <dfilatov@yandex-team.ru>
  * @description This module provides some syntax sugar for "class" declarations, constructors, mixins, "super" calls and static members.
  */
 
 modules.define('inherit', function(provide) {
 
-var hasIntrospection = (function(){'_';}).toString().indexOf('_') > -1,
-    emptyBase = function() {},
+var noop = function() {},
+    hasOwnProperty = Object.prototype.hasOwnProperty,
     objCreate = Object.create || function(ptp) {
         var inheritance = function() {};
         inheritance.prototype = ptp;
@@ -15,13 +17,13 @@ var hasIntrospection = (function(){'_';}).toString().indexOf('_') > -1,
     objKeys = Object.keys || function(obj) {
         var res = [];
         for(var i in obj) {
-            obj.hasOwnProperty(i) && res.push(i);
+            hasOwnProperty.call(obj, i) && res.push(i);
         }
         return res;
     },
     extend = function(o1, o2) {
         for(var i in o2) {
-            o2.hasOwnProperty(i) && (o1[i] = o2[i]);
+            hasOwnProperty.call(o2, i) && (o1[i] = o2[i]);
         }
 
         return o1;
@@ -33,11 +35,10 @@ var hasIntrospection = (function(){'_';}).toString().indexOf('_') > -1,
     isFunction = function(obj) {
         return toStr.call(obj) === '[object Function]';
     },
-    noOp = function() {},
     needCheckProps = true,
     testPropObj = { toString : '' };
 
-for(var i in testPropObj) { // fucking ie hasn't toString, valueOf in for
+for(var i in testPropObj) { // It's a pity ie hasn't toString, valueOf in for
     testPropObj.hasOwnProperty(i) && (needCheckProps = false);
 }
 
@@ -65,16 +66,26 @@ function override(base, res, add) {
         }
         prop = add[name];
         if(isFunction(prop) &&
-                (!hasIntrospection || prop.toString().indexOf('.__base') > -1)) {
+                (!prop.prototype || !prop.prototype.__self) && // check to prevent wrapping of "class" functions
+                (prop.toString().indexOf('.__base') > -1)) {
             res[name] = (function(name, prop) {
-                var baseMethod = base[name] || noOp;
-                return function() {
-                    var baseSaved = this.__base;
-                    this.__base = baseMethod;
-                    var res = prop.apply(this, arguments);
-                    this.__base = baseSaved;
-                    return res;
-                };
+                var baseMethod = base[name]?
+                        base[name] :
+                        name === '__constructor'? // case of inheritance from plain function
+                            res.__self.__parent :
+                            noop,
+                    result = function() {
+                        var baseSaved = this.__base;
+
+                        this.__base = result.__base;
+                        var res = prop.apply(this, arguments);
+                        this.__base = baseSaved;
+
+                        return res;
+                    };
+                result.__base = baseMethod;
+
+                return result;
             })(name, prop);
         } else {
             res[name] = prop;
@@ -97,25 +108,29 @@ function applyMixins(mixins, res) {
 }
 
 /**
- * Creates class.
- * @exports
- * @param {Function|Array} [baseClass|baseClassAndMixins] class (or class and mixins) for inherit from
- * @param {Object} prototypeFields
- * @param {Object} [staticFields]
- * @returns {Function} class
- */
-var inherit = function() {
+* Creates class
+* @exports
+* @param {Function|Array} [baseClass|baseClassAndMixins] class (or class and mixins) to inherit from
+* @param {Object} prototypeFields
+* @param {Object} [staticFields]
+* @returns {Function} class
+*/
+function inherit() {
     var args = arguments,
         withMixins = isArray(args[0]),
         hasBase = withMixins || isFunction(args[0]),
-        base = hasBase? withMixins? applyMixins(args[0]) : args[0] : emptyBase,
+        base = hasBase? withMixins? applyMixins(args[0]) : args[0] : noop,
         props = args[hasBase? 1 : 0] || {},
         staticProps = args[hasBase? 2 : 1],
-        res = props.__constructor || (hasBase && base.prototype.__constructor)?
+        res = props.__constructor || (hasBase && base.prototype && base.prototype.__constructor)?
             function() {
                 return this.__constructor.apply(this, arguments);
             } :
-            function() {};
+            hasBase?
+                function() {
+                    return base.apply(this, arguments);
+                } :
+                function() {};
 
     if(!hasBase) {
         res.prototype = props;
@@ -124,6 +139,8 @@ var inherit = function() {
     }
 
     extend(res, base);
+
+    res.__parent = base;
 
     var basePtp = base.prototype,
         resPtp = res.prototype = objCreate(basePtp);
@@ -134,7 +151,7 @@ var inherit = function() {
     staticProps && override(base, res, staticProps);
 
     return res;
-};
+}
 
 inherit.self = function() {
     var args = arguments,
