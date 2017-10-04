5003038135cc44b64300a4c095e644741fa65d13
Dmitry Filatov <dfilatov@yandex-team.ru>
2017-10-04 13:18:16 +0300

    i-bem__dom: Prevents memory leak with `domNodesToParents`

--- common.blocks/i-bem/__dom/i-bem__dom.js
+++ common.blocks/i-bem/__dom/i-bem__dom.js
@@ -1035,6 +1035,9 @@ DOM = BEM.decl('i-bem__dom',/** @lends BEMDOM.prototype */{
             });
             delete domElemToParams[identify(domNode)];
         });
+
+        // flush parent nodes storage that has been filled above
+        domNodesToParents = {};
     },
 
     /**
