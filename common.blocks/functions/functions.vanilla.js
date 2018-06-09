e13e569f77d1a6eccd3af4fb8bfe7edd162e74c5
Vladimir Grinenko <i@tadatuta.com>
2018-06-09 12:23:45 +0300

    isFunction: Fix incorrect behaviour for async functions

--- common.blocks/functions/functions.vanilla.js
+++ common.blocks/functions/functions.vanilla.js
@@ -14,7 +14,10 @@ provide(/** @exports */{
      * @returns {Boolean}
      */
     isFunction : function(obj) {
-        return toStr.call(obj) === '[object Function]';
+        // In some browsers, typeof returns "function" for HTML <object> elements
+        // (i.e., `typeof document.createElement( "object" ) === "function"`).
+        // We don't want to classify *any* DOM node as a function.
+        return typeof obj === 'function' && typeof obj.nodeType !== 'number';
     },
 
     /**
