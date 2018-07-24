0dffe4acaeb52754eaeca7be060a8e78fde6db33
Vladimir Grinenko <i@tadatuta.com>
2018-07-24 18:02:21 +0300

    Falling spec

--- common.blocks/i-bem-dom/__events/_type/i-bem-dom__events_type_dom.spec.js
+++ common.blocks/i-bem-dom/__events/_type/i-bem-dom__events_type_dom.spec.js
@@ -7,7 +7,7 @@ var undef,
     expect = chai.expect;
 
 describe('DOM events', function() {
-    var Block1, Block2, Block3, block1, spy1, spy2, spy3, spy4, spy5, spy6, spy7, spy8,
+    var Block1, Block2, Block3, block1, spy1, spy2, spy3, spy4, spy5, spy6, spy7, spy8, spy9,
         wrapSpy = function(spy) {
             return function(e) {
                 // NOTE: we need to pass bemTarget and data explicitly, as `e` is being
@@ -26,6 +26,7 @@ describe('DOM events', function() {
         spy6 = sinon.spy();
         spy7 = sinon.spy();
         spy8 = sinon.spy();
+        spy9 = sinon.spy();
     });
 
     afterEach(function() {
@@ -1030,6 +1031,24 @@ describe('DOM events', function() {
                         spy3.should.have.been.called;
                     });
                 });
+            });
+        });
+
+        describe('window events', function() {
+            beforeEach(function() {
+                Block1 = bemDom.declBlock('block1', {}, {
+                    onInit : function() {
+                        this._domEvents(window).on('resize', spy9);
+                    }
+                });
+
+                block1 = initDom({ block : 'block1' }).bem(Block1);
+            });
+
+            it('should properly bind handlers', function() {
+                bemDom.win.trigger('resize');
+
+                spy9.should.have.been.called;
             });
         });
     });
