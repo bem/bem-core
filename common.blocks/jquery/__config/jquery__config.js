8d270e45125aca9ff00690a615330daad2c9926c
Vladimir Grinenko <i@tadatuta.com>
2018-10-24 14:41:32 +0300

    Update jquery to 3.3.1

--- common.blocks/jquery/__config/jquery__config.js
+++ common.blocks/jquery/__config/jquery__config.js
@@ -10,7 +10,7 @@ provide(/** @exports */{
      * URL for loading jQuery if it does not exist
      * @type {String}
      */
-    url : 'https://yastatic.net/jquery/3.2.1/jquery.min.js'
+    url : 'https://yastatic.net/jquery/3.3.1/jquery.min.js'
 });
 
 });
